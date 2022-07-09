2e060a43a8b008ceb138e3864ad4602539fa66b5
Rase01 <c_sbravo@outlook.com>
2022-07-09 12:18:21 -0500

    commit

--- assets/correo.php
+++ assets/correo.php
@@ -1,17 +1,42 @@
 <?php
 
-$destino = 'c_sbravo@outlook.com';
-//correo de la empresa.
-
 $nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 $tipo = $_POST['tipo'];
-$correo = $_POST['correo'];
 
-$header = "Enviado desde TAVA PERU.";
-$asuntoCompleto = $tipo . "Motivo: " . $asunto;
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-mail($destino, $asuntoCompleto, $mensaje, $header);
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-?>
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'c_sbravo@outlook.com';                     //SMTP username
+    $mail->Password   = 'Cas.cxDS23z';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($correo, $nombre);
+    $mail->addAddress('c_sbravo@outlook.com');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Motivo del mensaje: '.$tipo.'. '.$asunto;
+    $mail->Body    = $mensaje;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
